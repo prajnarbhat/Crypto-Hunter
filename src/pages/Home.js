3d4772fbcce253219d0774b569a653f7da257b65
prajnarbhat <rbhatprajna@gmail.com>
2025-04-25 19:42:09 +0530

    Adding table content

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,40 +1,79 @@
+import { useContext, useEffect, useState } from "react";
+import CoinContext from "../context/CoinContext";
 const Home = () => {
+
+    const { allCoins, currency} = useContext(CoinContext);
+    const [ displayCoin, setDisplayCoin] = useState([])
+
+    console.log("Coins in home:", displayCoin)
+
+    useEffect(() => {
+        setDisplayCoin(allCoins);
+    },[allCoins])
 
     return (
         <>
             <div className="bg-gray-600 min-h-screen flex flex-col items-center">
-            <div className="flex flex-col max-w-[600px] items-center mt-20 p-6 gap-10 text-center">
-                <h2 className="text-3xl text-white">Largest Crypto Marketplace</h2>
+                <div className="flex flex-col max-w-[600px] items-center mt-20 p-6 gap-10 text-center">
+                    <h2 className="text-3xl text-white">Largest Crypto Marketplace</h2>
+                    <p className="text-white">
+                    Welcome to the world's largest cryptocurrency marketplace. 
+                    Sign up to explore more about cryptos.
+                    </p>
 
-                <p className="text-white">
-                Welcome to the world's largest cryptocurrency marketplace. 
-                Sign up to explore more about cryptos.
-                </p>
+                    <form className="flex gap-2">
+                        <input type="text" placeholder="Search Crypto"
+                            className="px-4 py-2 rounded-md"
+                        />
+                        <button type="submit" className="bg-blue-500 text-white px-4 py-2 rounded-md"
+                        >
+                            Search
+                        </button>
+                    </form>
+                </div>
 
-                <form className="flex gap-2">
-                <input type="text" placeholder="Search Crypto"
-                    className="px-4 py-2 rounded-md"
-                />
-                <button type="submit" className="bg-blue-500 text-white px-4 py-2 rounded-md"
-                >
-                    Search
-                </button>
-                </form>
-            </div>
-            <div>
-            <table border="1" className="w-full text-sm text-left rtl:text-right bg-white">
-                    <thead className="uppercase dark:text-black-900">
-                    <tr>
-                        <th scope="col" className="px-4 py-3"> # </th>
-                        <th scope="col" className="px-4 py-3"> Coins </th>
-                        <th scope="col" className="px-4 py-3"> Price </th>
-                        <th scope="col" className="px-4 py-3"> 24H Change </th>
-                        <th scope="col" className="px-4 py-3"> Market Cap </th>
-                    </tr>
-                    </thead>
-                    
-                </table>
-            </div>
+                <div className="overflow-x-auto bg-gray-100 m-4 rounded-lg shadow-md">
+                    <table border="1" className="w-full text-sm text-left rtl:text-right bg-white border border-gray-300 rounded-lg">
+                        <thead className="bg-gray-200 text-gray-700 uppercase">
+                            <tr>
+                                <th scope="col" className="px-6 py-3"> # </th>
+                                <th scope="col" className="px-6 py-3"> Coins </th>
+                                <th scope="col" className="px-6 py-3"> Price </th>
+                                <th scope="col" className="px-6 py-3"> 24H Change </th>
+                                <th scope="col" className="px-6 py-3"> Market Cap </th>
+                            </tr>
+                        </thead>
+                        <tbody className="w-100 divide-y divide-gray-300">
+                            {displayCoin.length > 0 ? (
+                                displayCoin.slice(0,10).map((item,index) => (
+                                    <tr key={index} className="hover:bg-gray-300 transition-colors duration-200">
+                                        <td className="px-6 py-3"> {item.market_cap_rank} </td>
+                                        <td className="px-6 py-3"> <img src={item.image} alt="img" className="w-6 h-6 rounded-full"/>
+                                            <span className="text-gray-900">{item.name} <span className="uppercase text-gray-500">({item.symbol})</span></span> </td>
+                                        <td className="px-6 py-4 text-green-600 font-semibold">
+                                                {currency.symbol} {item.current_price.toLocaleString()}
+                                        </td>
+                                        <td className={`px-6 py-4 font-medium ${
+                                            item.price_change_percentage_24h >= 0 ? 'text-green-500' : 'text-red-500'
+                                        }`}>
+                                            {item.price_change_percentage_24h.toFixed(2)}%
+                                        </td>
+
+                                        <td className="px-6 py-4 text-gray-800">
+                                            {item.market_cap.toLocaleString()}
+                                        </td>
+                                    </tr>   
+                                ))
+                            ) : (
+                                <tr>
+                                    <td colSpan="5" className="px-6 py-4 text-center text-lg text-gray-500">
+                                        No data found
+                                    </td>
+                                </tr>
+                            )}
+                        </tbody>
+                    </table>
+                </div>
             </div>
 
         </>
