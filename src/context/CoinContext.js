babb43a210975b5c5ad97893586ab1d2057fbbc5
prajnarbhat <rbhatprajna@gmail.com>
2025-04-26 20:13:03 +0530

    Changes to context file

--- src/context/CoinContext.js
+++ src/context/CoinContext.js
@@ -1,57 +1,46 @@
 // Store and retrieve data from localStorage
 // Provide global access using React Context
 
-import { createContext, useEffect, useState } from "react";
+import { createContext, useCallback, useEffect, useState } from "react";
 
 export const CoinContext = createContext();
 
 export const CoinContextProvider = ({children}) => {
 
-    const [ allCoins, setAllCoins] = useState([])
-    // initial the currency will be USD
-    const [ currency, setCurrency] = useState( {
+    const [allCoins, setAllCoins] = useState([]);
+    const [currency, setCurrency] = useState({
         name: "usd",
         symbol: "$"
-    })
+    });
 
-    const fetchAllCoins = async() => {
+    const fetchAllCoins = useCallback(async () => {
         const options = {
             method: 'GET',
-            headers: {accept: 'application/json', 'x-cg-demo-api-key': 'CG-JcGA7m5bu7h3HmvkbNcucbFe'}
+            headers: { accept: 'application/json', 'x-cg-demo-api-key': 'CG-JcGA7m5bu7h3HmvkbNcucbFe' }
         };
-        
-        fetch(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency.name}`, options)
-            .then(res => res.json())
-            .then(res => setAllCoins(res))
-            .catch(err => console.error(err));
-    }
 
-    // whenever the currency changes it will call this new fetch data
+        try {
+            const response = await fetch(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency.name}`, options);
+            const data = await response.json();
+            setAllCoins(data);
+        } catch (error) {
+            console.error(error);
+        }
+    }, [currency.name]); 
 
     useEffect(() => {
-        fetchAllCoins()
-    },[currency, fetchAllCoins])
-
-    console.log("Currency value is:", currency);
-    console.log("Setcurrency is:", setCurrency)
-
+        fetchAllCoins();
+    }, [fetchAllCoins]); 
 
     const contextValue = {
         allCoins, currency, setCurrency
-    }
+    };
 
     return (
-         <CoinContext.Provider value={contextValue}>
+        <CoinContext.Provider value={contextValue}>
             {children}
- 
         </CoinContext.Provider>
-    )
-
-
-    
-
-
-
-}
+    );
+};
 
 export default CoinContext;
